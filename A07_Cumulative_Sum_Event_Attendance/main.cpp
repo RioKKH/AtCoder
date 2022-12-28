3e8b1f967d0c033c496484246e8dd466003ffafe
Ryoichi Kakehi <ryoichikakehi@gmail.com>
2022-12-28 23:23:10 +0900

    new file:   A07_Cumulative_Sum_Event_Attendance/main.cpp 	new file:   A07_Cumulative_Sum_Event_Attendance/test/sample-1.in 	new file:   A07_Cumulative_Sum_Event_Attendance/test/sample-1.out

---
+++ A07_Cumulative_Sum_Event_Attendance/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int N, D;
+int L[100000];
+int R[100000];
+
+int main()
+{
+    std::cin >> D >> N;
+
+    for (int i=0; i < N; i++)
+    {
+        std::cin >> L[i] >> R[i];
+    }
+
+    return 0;
+}
+
