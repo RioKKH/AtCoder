9c675954acf2568dbb37acfc8468e11b1ae2a98e
kakehi <kakehi.ryoichi@nuflare.co.jp>
2024-12-24 12:47:56 +0900

    modified:   .gitignore 	new file:   A-Disjoint_Set_Union/Makefile 	new file:   A-Disjoint_Set_Union/compile_commands.json 	new file:   A-Disjoint_Set_Union/main.cpp 	modified:   "A15_Compression_\345\272\247\346\250\231\344\272\234\345\234\247\347\270\256/main.cpp"

---
+++ A-Disjoint_Set_Union/main.cpp
@@ -0,0 +1,4 @@
+#include <atcoder/dsu>
+#include <iostream>
+
+int main() { return 0; }
--- A15_Compression_座標亜圧縮/main.cpp
+++ A15_Compression_座標亜圧縮/main.cpp
@@ -1,25 +1,26 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int main()
-{
+int main() {
     // 座標圧縮を扱う問題
     // 座標圧縮ではソート、ユニークな要素の抽出、二分探索を用いるので
     // これら手法を組み合わせた総合問題となっている
 
-    int N; // 与えられる配列の長さ
+    int N;          // 与えられる配列の長さ
     int A[100'009]; // 圧縮前のデータを格納する配列
     int B[100'009]; // 圧縮後のデータを格納する配列
     vector<int> T;
 
     // 入力
     cin >> N;
-    for (int i = 1; i <= N; i++) cin >> A[i];
+    for (int i = 1; i <= N; i++)
+        cin >> A[i];
 
     // 配列Ｔの作成
-    for (int i = 1; i <= N; i++) T.push_back(A[i]);
+    for (int i = 1; i <= N; i++)
+        T.push_back(A[i]);
     sort(T.begin(), T.end());
 
     // 配列Ｔの重複を消す
@@ -29,17 +30,17 @@ int main()
     // std::vector::erase()
     // 指定した要素を削除する
     // T.erase(T.begin() + 2); // 2番目の単一要素を削除
-    // T.erase(T.begin(), T.begin() + 2); begin() ~ beging() + 2の範囲の要素を削除
+    // T.erase(T.begin(), T.begin() + 2); begin() ~ beging() +
+    // 2の範囲の要素を削除
     //
     // std::unique()
     // decltype(T)::iterator result = std::unique(T.begin(), T.end())
     // [T.begin(), result)の範囲に重複を除いた結果が入っている。
     // 不要になった要素を削除する
     // T.erase(result, T.end()); // これを1行で実装したのが23行目
-    
+
     // 答えを求める
-    for (int i = 1; i <= N; i++)
-    {
+    for (int i = 1; i <= N; i++) {
         // vector型のlower_boundは以下のような形式で各
         // vector型の添え字は0番目から始まるので、1を足す必要があることに注意
         // 数値A[i]が配列Tの何番目の数値と一致するかを二分探索で調べる。
@@ -50,9 +51,9 @@ int main()
     }
 
     // 答えを空白区切りで出力
-    for (int i = 1; i <= N; i++)
-    {
-        if (i >= 2) cout << " ";
+    for (int i = 1; i <= N; i++) {
+        if (i >= 2)
+            cout << " ";
         cout << B[i];
     }
     cout << endl;
