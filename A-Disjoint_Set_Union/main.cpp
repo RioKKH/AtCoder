5043b18ea80717faa8c55b0b282d2c1e81b0dce4
kakehi <kakehi.ryoichi@nuflare.co.jp>
2024-12-24 13:05:01 +0900

    modified:   main.cpp

--- A-Disjoint_Set_Union/main.cpp
+++ A-Disjoint_Set_Union/main.cpp
@@ -1,4 +1,25 @@
 #include <atcoder/dsu>
 #include <iostream>
+#include <vector>
 
-int main() { return 0; }
+void show(const std::vector<int> &V) {
+    for (const auto &v : V) {
+        std::cout << v << std::endl;
+    }
+}
+
+int main() {
+    int N, Q;
+    std::cin >> N >> Q;
+    std::vector<int> t(Q), u(Q), v(Q);
+
+    for (int i = 0; i < Q; i++) {
+        std::cin >> t.at(i) >> u.at(i) >> v.at(i);
+    }
+
+    show(t);
+    show(u);
+    show(v);
+
+    return 0;
+}
