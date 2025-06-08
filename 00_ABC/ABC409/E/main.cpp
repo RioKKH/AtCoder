035b1554e9d1adf37425c5a63c4b742a1f04c811
Ryoichi <ryoichikakehi@gmail.com>
2025-06-08 22:18:27 +0900

    modified:   00_ABC/ABC409/E/main.cpp

--- 00_ABC/ABC409/E/main.cpp
+++ 00_ABC/ABC409/E/main.cpp
@@ -28,13 +28,17 @@ int main()
     ll ans = 0;
     auto dfs = [&](auto dfs, int v, int p = -1) -> int
     {
-        int tot = x[v];
+        int tot = x[v]; // 現在の頂点vの初期電化
+        // 全ての隣接頂点を処理
         for (auto e : g[v])
         {
-            if (e.to == p)
+            if (e.to == p) // 親ノードは除外する
                 continue;
+            // 子ノードを再帰的に処理する
             int r = dfs(dfs, e.to, v);
+            // 子ノードからの余剰電化を移動するコストを追加
             ans += (ll)e.cost * abs(r);
+            // 子ノードの余剰電荷を現在のノードに統合する
             tot += r;
         }
         return tot;
