01312286e1d369635182a428a585103b2e2aef45
Ryoichi <ryoichikakehi@gmail.com>
2026-01-25 02:06:02 +0900

    多重for文 - ペア・タプルを走査 - 6Q - TAKOYAKI FESTIVAL 2019 	new file:   00_ABC/ABC143/B/Makefile 	new file:   00_ABC/ABC143/B/main.cpp

---
+++ 00_ABC/ABC143/B/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    int total = 0;
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            total += A[i] * A[j];
+        }
+    }
+    cout << total << endl;
+    return 0;
+}
