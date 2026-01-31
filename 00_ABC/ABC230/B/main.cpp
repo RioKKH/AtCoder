d2c51267965686d08495ccd4641a1d58c0dc42ef
Ryoichi <ryoichikakehi@gmail.com>
2026-02-01 01:00:12 +0900

    全探索 - 全探索の基本 - 6Q - Triple Metre 	new file:   00_ABC/ABC230/B/Makefile 	new file:   00_ABC/ABC230/B/main.cpp

---
+++ 00_ABC/ABC230/B/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    string T = "";
+    for (int i = 0; i < 50; i++)
+    {
+        T += "oxx";
+    }
+
+    if (T.find(S) != string::npos)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
