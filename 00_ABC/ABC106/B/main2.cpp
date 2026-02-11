895c3ba7df1d1fedd1c390b3ddc6e49ea9a99ea0
Ryoichi <ryoichikakehi@gmail.com>
2026-02-11 15:18:34 +0900

    全探索 - 整数値を全探索 - 6Q - 105 	new file:   00_ABC/ABC106/B/Makefile 	new file:   00_ABC/ABC106/B/main.cpp 	new file:   00_ABC/ABC106/B/main1.cpp 	new file:   00_ABC/ABC106/B/main2.cpp

---
+++ 00_ABC/ABC106/B/main2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int answer = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        int count = 0;
+        for (int j = 1; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                count++;
+                if (j != i / j)
+                {
+                    count++;
+                }
+                // cout << "i:" << i << " j:" << j << " count:" << count << endl;
+            }
+        }
+        if (i % 2 == 1 && count == 8)
+        {
+            answer++;
+        }
+    }
+
+    cout << answer << endl;
+    return 0;
+}
