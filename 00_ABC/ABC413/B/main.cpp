604d8ed9bb91d1ac1b7288a0c91f0022b59f903c
Ryoichi <ryoichikakehi@gmail.com>
2026-03-29 18:52:35 +0900

    集合 - 6Q - ABC413-B - cat 2 	new file:   00_ABC/ABC413/B/Makefile 	new file:   00_ABC/ABC413/B/main.cpp

---
+++ 00_ABC/ABC413/B/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print_vector(const vector<T> &v)
+{
+    int size = v.size();
+    for (int i = 0; i < size; i++)
+    {
+        cout << v[i];
+        if (i < size - 1)
+        {
+            cout << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<string> S(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> S[i];
+    }
+
+    set<string> SS;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (i == j)
+            {
+                // skip;
+            }
+            else
+            {
+                SS.insert(S[i] + S[j]);
+            }
+        }
+    }
+    cout << SS.size() << endl;
+    return 0;
+}
