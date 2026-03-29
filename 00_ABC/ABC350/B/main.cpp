d09be0175ecdff998c0f9962118a755ed14c30f2
Ryoichi <ryoichikakehi@gmail.com>
2026-03-29 15:05:55 +0900

    バケット - 6Q - ABC350-B : Dentist Aoki 	modified:   00_ABC/ABC350/B/main.cpp

--- 00_ABC/ABC350/B/main.cpp
+++ 00_ABC/ABC350/B/main.cpp
@@ -24,6 +24,23 @@ int main()
     // i番目の要素に歯が生えているかどうか
     // true: 生えてる
     // false: 抜けている
-    vector<bool> T(N, true);
+    vector<bool> T(N + 1, true);
+
+    // クエリをvectorに保存しておく
+    vector<int> S(Q, 0);
+    for (int i = 0; i < Q; i++)
+    {
+        cin >> S[i];
+    }
+
+    for (int i = 0; i < Q; i++)
+    {
+        T[S[i]] = !T[S[i]]; // boolを反転させる vector<bool>.flip()もある
+    }
+    // print_vector(T);
+    // 0番目の要素は除外する。
+    int answer = std::count(T.begin() + 1, T.end(), true);
+    cout << answer << endl;
+
     return 0;
 }
