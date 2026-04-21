c2c2a83be0c6dbce4c90bf82f1a2574b31d05c3d
Ryoichi <ryoichikakehi@gmail.com>
2026-04-21 15:55:59 +0900

    グラフ - 5Q - ABC276-B - Adjacency List 	new file:   00_ABC/ABC276/B/Makefile 	new file:   00_ABC/ABC276/B/main.cpp

---
+++ 00_ABC/ABC276/B/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void print_vector(const vector<T> &v)
+{
+    int size = v.size();
+    for (int i = 0; i < size; i++)
+    {
+        cout << v[i];
+        if (i < size - 1)
+        {
+            cout << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    // N個の都市
+    // M本の道路
+    vector<vector<int>> G(N + 1);
+    for (int i = 1; i <= M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        sort(G[i].begin(), G[i].end());
+        cout << G[i].size();
+        for (const auto g : G[i])
+        {
+            cout << " " << g;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
