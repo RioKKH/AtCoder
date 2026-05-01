6597cc74a4c6916945be5849f6b3cba9bb3e25de
Ryoichi <ryoichikakehi@gmail.com>
2026-05-02 02:03:42 +0900

    DFS - 3Q - ABC177-D - Friends 	modified:   00_ABC/ABC177/D/main.cpp

--- 00_ABC/ABC177/D/main.cpp
+++ 00_ABC/ABC177/D/main.cpp
@@ -1,22 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-void print_vector(const vector<T> &v)
+void dfs(int i,
+         const vector<vector<int>> &graph,
+         vector<bool> &visited,
+         int &size)
 {
-    int size = v.size();
-    for (int i = 0; i < size; i++)
+    visited[i] = true;
+    size++;
+
+    for (const auto &g : graph[i])
     {
-        cout << v[i];
-        if (i < size - 1)
+        if (!visited[g])
         {
-            cout << " ";
+            dfs(g, graph, visited, size);
         }
     }
-    cout << endl;
 }
 
 int main()
 {
+    int N, M;
+    cin >> N >> M;
+
+    // 交友関係を表現するグラフを隣接リスト方式で作成する
+    vector<vector<int>> G(N + 1);
+    for (int i = 0; i < M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    // グラフを走査して連結成分を探索する。各連結成分のサイズを管理する。
+    vector<bool> visited(N + 1, false);
+    int max_size = 0; // 連結成分の最大サイズ
+    for (int i = 1; i <= N; i++)
+    {
+        if (!visited[i])
+        {
+            int size = 0;
+            dfs(i, G, visited, size);
+            max_size = max(max_size, size);
+        }
+    }
+    cout << max_size << endl;
     return 0;
 }
