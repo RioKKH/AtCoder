b5ca2c8609254464363baef04549f06cd5c4171c
Ryoichi <ryoichikakehi@gmail.com>
2026-05-05 23:50:17 +0900

    BFS - 3Q - 07_math_and_algorithm/044 - Shortest Path 1 	modified:   07_math_and_algorithm/044/main.cpp

--- 07_math_and_algorithm/044/main.cpp
+++ 07_math_and_algorithm/044/main.cpp
@@ -1,19 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-void print_vector(const vector<T> &v)
+int bfs()
 {
-    int size = v.size();
-    for (int i = 0; i < size; i++)
-    {
-        cout << v[i];
-        if (i < size - 1)
-        {
-            cout << " ";
-        }
-    }
-    cout << endl;
+    return 0;
 }
 
 int main()
