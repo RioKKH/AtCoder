a2152c85570c44b717d81a89ffe71874b31e821a
Ryoichi <ryoichikakehi@gmail.com>
2026-05-06 23:30:30 +0900

    再帰関数 - 7Q - ABC273-A - A Recursive Function 	new file:   00_ABC/ABC273/A/Makefile 	new file:   00_ABC/ABC273/A/main.cpp

---
+++ 00_ABC/ABC273/A/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int x)
+{
+    if (x == 0)
+    {
+        return 1;
+    }
+    return x * f(x - 1);
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int answer = f(N);
+
+    cout << answer << endl;
+    return 0;
+}
