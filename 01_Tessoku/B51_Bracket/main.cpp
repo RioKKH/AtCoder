cfcbfff39727a4be8b9e050cc642553f8720caef
Ryoichi <ryoichikakehi@gmail.com>
2026-05-21 00:14:30 +0900

    スタック - Tessoku-B51 - Bracket 	modified:   01_Tessoku/B51_Bracket/main.cpp

--- 01_Tessoku/B51_Bracket/main.cpp
+++ 01_Tessoku/B51_Bracket/main.cpp
@@ -11,13 +11,34 @@ int main()
     // 対応するペアを記録するリスト
     vector<pair<int, int>> pairs;
 
-    for (int i = 0; i < S.size(); i++)
+    for (int i = 0; i < static_cast<int>(S.size()); i++)
     {
         if (S[i] == '(')
         {
             // '('を見つけたらその位置をpush (1-indexed)
             stck.push(i + 1);
         }
+        else if (S[i] == ')')
+        {
+            int left = stck.top();
+            int right = i + 1; // 1-indexed
+            stck.pop();
+
+            // ペアを記録する どちらの形でもOK
+            pairs.push_back({left, right});
+            // pairs.emplace_back(left, right);
+        }
+    }
+
+    // 本来はソートする必要はないが、出力形式がvectorに入れた順ではない場合
+    sort(pairs.begin(), pairs.end(),
+         [](auto &a, auto &b)
+         { return a.second < b.second; });
+
+    // 回答出力
+    for (const auto &[l, r] : pairs)
+    {
+        cout << l << " " << r << endl;
     }
     return 0;
 }
