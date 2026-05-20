b17109dee7b146ab6a2eb254c8e9b4ff45f47a2e
Ryoichi <ryoichikakehi@gmail.com>
2026-05-20 23:55:19 +0900

    スタック - Tessoku-B51 - Bracket 	modified:   01_Tessoku/B51_Bracket/main.cpp

--- 01_Tessoku/B51_Bracket/main.cpp
+++ 01_Tessoku/B51_Bracket/main.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 int main()
 {
+    string S;
+    cin >> S;
+
+    // '('の位置を記録するスタック
+    stack<int> stck;
+    // 対応するペアを記録するリスト
+    vector<pair<int, int>> pairs;
+
+    for (int i = 0; i < S.size(); i++)
+    {
+        if (S[i] == '(')
+        {
+            // '('を見つけたらその位置をpush (1-indexed)
+            stck.push(i + 1);
+        }
+    }
     return 0;
 }
